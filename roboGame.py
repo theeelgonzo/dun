7371943347a5b4513f9ebf4b64d3df34458fee93
Jordan Godwin <jordan.godwin13@gmail.com>
2023-11-10 17:51:49 -0600

    Define playGame func with player instantiation.

--- roboGame.py
+++ roboGame.py
@@ -3,11 +3,11 @@ import classes.py as ro
 # intialize game and start main game loop
 
 def playGame:
-    pass
+    player = ro.MobileUnit("Combatant", 10, 5, 0, 10, [], weaponOne = Weapon("laser", 0, 10, 2, 999999, 3), weaponTwo = Weapon("cannon", 0, 20, 1, 50, 5))
 
 def gameStart:
     game = ro.Game(True)
-    while game == True:
+    while game == True
         playGame()
     else:
         sys.exit()

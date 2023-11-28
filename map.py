2c868374b81111ec0bc2df9703b67ff8df9f7cd2
Jordan Godwin <jordan.godwin13@gmail.com>
2023-11-28 13:31:15 -0600

    Add introduceSelf function.
    Room Class can have variable amount of doors and can track and report
    the number of doors available to players.

--- map.py
+++ map.py
@@ -1,27 +1,32 @@
 # Game Map for Dun 2
 
-import attack.py as atk
+# import attack.py as atk
 
 class Room:
-    def __init__(self, name, doors, monster, trap, secret):
+    def __init__(self, name, doors, monster, trap, secret, pcHasVisited):
         self.name = name
         self.doors = doors
         self.monster = monster
         self.trap = trap
         self.secret = secret
+        self.pcHasVisited = pcHasVisited
 
     def introduceSelf(self):
-        numDoors = len(doors)
+        numDoors = len(self.doors)
         nameDoors = []
-        for door in doors:
+        for door in self.doors:
             nameDoors.append(door.name)
+            #print(door.name)
         print(f"There are {numDoors} doors in this room.")
         print(*nameDoors)
 
 class Door:
-    def __init__(self, name, destination):
+    def __init__(self, name, destination, isLocked):
         self.name = name
+        self.destination = destination
+        self.isLocked = isLocked
 
 
-newRoom = Room("Entry Chamber", )
+newRoom = Room("Entry Chamber", [Door("A Door to the North", "north"), Door("A door to the south", "south")], None, None, None)
 
+newRoom.introduceSelf()

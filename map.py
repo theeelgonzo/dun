8679aa5ebabb63cd174ee0bac39b3b783a6f2d82
Jordan Godwin <jordan.godwin13@gmail.com>
2023-11-29 18:17:54 -0600

    add room one creation function

--- map.py
+++ map.py
@@ -2,6 +2,8 @@
 
 # import attack.py as atk
 
+dunRooms = []
+
 class Room:
     def __init__(self, name, doors, monster, trap, secret, pcHasVisited):
         self.name = name
@@ -26,69 +28,72 @@ class Door:
         self.destination = destination
         self.isLocked = isLocked
 
-
-newRoom = Room("Entry Chamber", [Door("A Door to the North", "north"), Door("A door to the south", "south")], None, None, None)
-
-newRoom.introduceSelf()
+def roomA1():
+    print(dunRooms)
+    a1 = Room("a1", [Door("North Door", "a2", False)], None, None, None, True)
+    dunRooms.append(a1)
+    list(set(dunRooms))
 
 # dungeon map switch set
 
 def goTo(destination):
     match destination:
-        case a1:
+        case "a1":
             roomA1()
-        case a2:
+        case "a2":
             roomA2()
-        case a3:
+        case "a3":
             roomA3()
-        case a4:
+        case "a4":
             roomA4()
-        case a5:
+        case "a5":
             roomA5()
-        case a6:
+        case "a6":
             roomA6()
-        case a7:
+        case "a7":
             roomA7()
-        case a8:
+        case "a8":
             roomA8()
-        case a9:
+        case "a9":
             roomA9()
-        case a10:
+        case "a10":
             roomA10()
-        case a11:
+        case "a11":
             roomA11()
-        case a12:
+        case "a12":
             roomA12()
-        case a13:
+        case "a13":
             roomA13()
-        case a14:
+        case "a14":
             roomA14()
-        case a15:
+        case "a15":
             roomA15()
-        case a16:
+        case "a16":
             roomA16()
-        case a17:
+        case "a17":
             roomA17()
-        case a18:
+        case "a18":
             roomA18()
-        case a19:
+        case "a19":
             roomA19()
-        case a20:
+        case "a20":
             roomA20()
-        case a21:
+        case "a21":
             roomA21()
-        case a22:
+        case "a22":
             roomA22()
-        case a23:
+        case "a23":
             roomA23()
-        case a24:
+        case "a24":
             roomA24()
-        case a25:
+        case "a25":
             roomA25()
-        case a26:
+        case "a26":
             roomA26()
-        case a27:
+        case "a27":
             roomA27()
         case _:
             print("negatory ghost rider")
-
+goTo("a1")
+print(dunRooms)
+goTo("a1")

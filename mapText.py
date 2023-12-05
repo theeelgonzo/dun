dfa15827dc32f74ed60a4f7939726290abb991f4
Jordan Godwin <jordan.godwin13@gmail.com>
2023-12-05 07:09:23 -0600

    Add map text module.

---
+++ mapText.py
@@ -0,0 +1,5 @@
+class RoomText():
+    def __init__(self, entryText, examineText, visitedText):
+        self.entryText = entryText
+        self.examineText = examineText
+        self.visitedText = visitedText
